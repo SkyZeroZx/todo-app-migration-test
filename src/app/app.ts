ac14e82d7bd5e6d5b4afa719f942c8e529dfa308
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2025-09-28 20:28:15 -0500

    add case custom commom module

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { Component, NgModule, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { CommonModule as NgCommomModule } from '@angular/common';
+
+@NgModule({
+  imports: [NgCommomModule],
+})
+export class CommonModule {}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule , RouterLink],
+  imports: [RouterOutlet, NgCommomModule, CommonModule, RouterLink],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('todo-app');
